c8e39f3f1bf32e40e9e422a49b0a344a3a59e9c5
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-07-18 23:02:16 -0300

    Inserindo uma Categoria no Banco Dados

--- src/main/java/br/com/dankicommerce/controller/FormCategoriaController.java
+++ src/main/java/br/com/dankicommerce/controller/FormCategoriaController.java
@@ -1,16 +1,44 @@
 package br.com.dankicommerce.controller;
 
+import javax.inject.Inject;
+import javax.validation.Valid;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.interceptor.IncludeParameters;
+import br.com.caelum.vraptor.validator.Validator;
+import br.com.dankicommerce.interceptors.SomenteLogado;
+import br.com.dankicommerce.model.Categoria;
+import br.com.olimposistema.aipa.dao.DAO;
 
 @Controller
 @Path("formcategoria")
 public class FormCategoriaController {
 	
+	@Inject Result result;
+	@Inject DAO<Categoria> categoriaDao;
+	@Inject Validator validator;
+	
+	
 	@Get("")
+	@SomenteLogado
 	public void formcategoria() {
 		// vai cair aqui
+	}
+	
+	@IncludeParameters
+	@SomenteLogado
+	@Post("salvaCategoria")
+	public void salvaCategoria(@Valid Categoria categoria) {
+		
+		validator.onErrorRedirectTo(this).formcategoria();
+		categoriaDao.insert(categoria);
+		
+		result.redirectTo(CategoriasController.class).categorias();
+		
 	}
 
 }
